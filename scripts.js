37fe4c8dceb86807252d83b6cfc7a2315c604722
MCM42 <mcmaya@gmx.net>
2025-02-15 11:51:08 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Die Seite wurde vollständig geladen.');
+    // Füge hier deine interaktiven JavaScript-Elemente hinzu
+});
